4e44158c5e939123fed825e197878a0b80e7b13b
Zaid Ebrahim <36286@ITCINFOTECH.com>
2021-01-09 00:27:49 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import Shopping from "./pages/shopping.page";
+
+function App() {
+  return (
+    <div className="App">
+      <Shopping />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/shopping.page.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./shopping.styles.css";
+
+const Shopping = (props) => {
+  return <div>Shopping Page</div>;
+};
+
+export default Shopping;
